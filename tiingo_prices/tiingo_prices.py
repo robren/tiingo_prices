64309e97f4c528e374425308bc639f031edc124b
Rob Rennison <rob@robren.net>
2019-01-31 22:43:17 -0500

    Fixed bug where the price returned was always the current days price and not the end_date where we added this optional parameter

--- tiingo_prices/tiingo_prices.py
+++ tiingo_prices/tiingo_prices.py
@@ -20,8 +20,8 @@ def main():
                 The date should be entered as YYYY-MM-DD \
                     trailing 12 months dividends are also relative to this date')
     parser.add_argument('--nyse_pref', action='store_true',
-            help='Attempt to recongnize NYSE Preferred ticker symbols and convert to tiingo friendly format. \
-            Warning may produce incorect results for non NYSE stocks with PR in their ticker name')
+            help='Attempt to recognize NYSE Preferred ticker symbols and convert to tiingo friendly format. \
+            Warning may produce incorrect results for non NYSE stocks with PR in their ticker name')
     parser.add_argument('--version', action='version', version='0.2.3')
     args = parser.parse_args()
     out_file = args.output_file
@@ -31,6 +31,7 @@ def main():
         end_date = date.fromisoformat(end_date)
     else:
         end_date = date.today()
+    print("end_date = {}".format(end_date))
 
     if out_file:
         if os.path.isfile(out_file):
@@ -73,9 +74,9 @@ def get_prices(ticker_file, tiingo_key, nyse_pref, end_date):
 
     Args:
         ticker_file: A file with a ticker per line.
-        tiingo_key: The tiingo API authentiaction key. End of day pricing data
+        tiingo_key: The tiingo API authentication key. End of day pricing data
         was free as of the time of writing Nov 2018.
-        nyse_pref: Attempt to recognixe tickers with PR in the ticker name and
+        nyse_pref: Attempt to recognize tickers with PR in the ticker name and
         convert to tiingo friendly format for the lookup.
         end_date: Date to get prices from.
     Returns:
@@ -116,16 +117,17 @@ def get_prices(ticker_file, tiingo_key, nyse_pref, end_date):
             else:
                 tiingo_ticker = ticker
             try:
-                price = client.get_ticker_price(tiingo_ticker)
+#                price = client.get_ticker_price(tiingo_ticker, startDate=end_date,endDate=end_date)
                 ly_hist = client.get_dataframe(tickers=tiingo_ticker,
                         frequency='daily',
                         startDate=start_date,
                         endDate=end_date)
                 tot_divs = ly_hist['divCash'].sum()
-                print("Ticker = {} Price  = {} Divs = {:.2f}"
-                        .format(ticker, price[0]['close'], tot_divs))
+                # Forced to use numerical indexing for the last row hence the cryptic -1,0
+                print("Ticker = {} Price  = {:.3f} Divs = {:.2f}"
+                        .format(ticker, ly_hist.iloc[-1,0], tot_divs))
                 prices.append((ticker,
-                    price[0]['adjClose'],
+                    round(ly_hist.iloc[-1,0],3),
                     round(tot_divs, 3)))
             except Exception as ex:
                 template = "An exception of type {0} occurred. Arguments:\n{1!r}"
